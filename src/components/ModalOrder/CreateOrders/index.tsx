efa03790fad1b86a0ae56b1cf3d643421e2588c3
SayuryKato <skdp.ecp19@uea.edu.br>
2024-08-20 14:41:24 -0400

    feat: Listagem de Vendas

--- src/components/ModalOrder/CreateOrders/index.tsx
+++ src/components/ModalOrder/CreateOrders/index.tsx
@@ -5,8 +5,43 @@ import ButtonAutolineCancel from '../../ButtonAutolineCancel';
 import ButtonSave from '../../ButtonSave';
 import { CiTrash } from 'react-icons/ci';
 import { CiSearch } from 'react-icons/ci';
+import axios_product from '../../../api/axios';
+import { useEffect, useState } from 'react';
+
+interface Customer {
+  id: number;
+  name: string;
+}
 
 export default function CreateOrders() {
+  const [customer, setCustomer] = useState<Customer[] | null>(null);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      try {
+        let url = 'product';
+        if (searchTerm) {
+          url = `product?search=${searchTerm}`;
+        }
+        const [responseCustomer, productResponse] = await Promise.all([
+          axios_product.get('v1/customer-find-all'),
+          axios_product.get(`v1/${url}`)
+        ]);
+
+        setCustomer(responseCustomer.data);
+        console.log(productResponse.data.data);
+
+        // Preencher os outros campos
+
+      } catch (error) {
+        console.error('Error fetching product details:', error);
+      }
+    };
+
+    fetchProductDetails();
+  }, []);
+
   return (
     <S.Container>
       <S.Content>
@@ -37,33 +72,40 @@ export default function CreateOrders() {
             <div>
               <label>Forma de Pagamento</label>
               <S.Filter id="selection" name="Filtro">
-                <option value="Filtos">Dinheiro</option>
-                <option value="opcao2">Pix</option>
-                <option value="opcao3">Cartão de Crédito</option>
-                <option value="opcao4">Cartão de Débito</option>
-                <option value="opcao5">Boleto</option>
-              </S.Filter>
-            </div>
-
-            <div>
-              <label>Parcelas</label>
-              <S.Filter id="selection" name="Filtro">
-                <option value="Filtos">1X 200,50</option>
-                <option value="opcao2">Parcela 2</option>
-                <option value="opcao3">Parcela 3</option>
-                <option value="opcao4">Parcela 4</option>
-                <option value="opcao5">Parcela 5</option>
+                <option value="Dinheiro">Dinheiro</option>
+                <option value="Pix">Pix</option>
+                <option value="Credito">Cartão de Crédito</option>
+                <option value="Debito">Cartão de Débito</option>
+                <option value="Boleto">Boleto</option>
               </S.Filter>
             </div>
 
             <div>
               <label>Cliente</label>
               <S.Filter id="selection" name="Filtro">
-                <option value="Filtos">Selecione</option>
-                <option value="opcao2">Cliente 2</option>
-                <option value="opcao3">Cliente 3</option>
-                <option value="opcao4">Cliente 4</option>
-                <option value="opcao5">Cliente 5</option>
+                <option value="" disabled selected hidden>
+                  Selecione
+                </option>
+                {
+                  customer?.map((val) => {
+                    return (
+                      <option key={val.id} value={val.id}>
+                        {val.name}
+                      </option>
+                    )
+                  })
+                }
+              </S.Filter>
+            </div>
+
+            <div>
+              <label>DESCONTO</label>
+              <S.Filter id="selection" name="Filtro">
+                <option value="Filtos">0%</option>
+                <option value="opcao2">Desconto 2</option>
+                <option value="opcao3">Desconto 3</option>
+                <option value="opcao4">Desconto 4</option>
+                <option value="opcao5">Desconto 5</option>
               </S.Filter>
             </div>
           </S.ContentLeft>
@@ -96,19 +138,6 @@ export default function CreateOrders() {
                       </button>
                     </td>
                   </tr><tr>
-                    <td>1</td>
-                    <td>7878465957863</td>
-                    <td>Bolsa</td>
-                    <td>1,000</td>
-                    <td>298,98</td>
-                    <td>298,98</td>
-                    <td>
-                      <button>
-                          <CiTrash />
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
                     <td>1</td>
                     <td>7878465957863</td>
                     <td>Bolsa</td>
@@ -168,25 +197,9 @@ export default function CreateOrders() {
               <div>
                 <label>SUBTOTAL</label>
                 <S.ValoresT>R$ 0,00</S.ValoresT>
+                <label>TOTAL</label>
+                <S.ValoresT>R$ 0,00</S.ValoresT>
               </div>
-
-              <S.DivDuple>
-                <S.InputLeft>
-                  <label>DESCONTO</label>
-                  <S.Filter id="selection" name="Filtro">
-                    <option value="Filtos">0%</option>
-                    <option value="opcao2">Desconto 2</option>
-                    <option value="opcao3">Desconto 3</option>
-                    <option value="opcao4">Desconto 4</option>
-                    <option value="opcao5">Desconto 5</option>
-                  </S.Filter>
-                </S.InputLeft>
-
-                <S.InputRight>
-                  <label>TOTAL</label>
-                  <S.ValoresT>R$ 0,00</S.ValoresT>
-                </S.InputRight>
-              </S.DivDuple>
             </S.FormTotal>
           </S.ContentRight>
         </S.ContainerBloc>
--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -4,93 +4,196 @@ import NewItem from '../../components/NewItem';
 import Pagination from '../../components/Pagination';
 import ModalDetails from '../../components/ModalProduct/Details';
 import { PiClipboardTextThin } from 'react-icons/pi';
-import { CiCirclePlus, CiTrash, CiEdit, CiCircleInfo } from 'react-icons/ci';
-import { useState } from 'react';
-import ModalConfirm from '../../components/ModalConfirm';
+import { CiCirclePlus, CiTrash, CiEdit } from 'react-icons/ci';
+import { useEffect, useState } from 'react';
+import axios_product from '../../api/axios';
+import Loading from '../../components/Loading';
+
+import { parseISO, format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+interface IData {
+  id: number,
+  created_at: string,
+  customer: Customer,
+  user: User,
+  products: Produto[],
+  discount: string
+}
+
+interface Customer {
+  id: number;
+  name: string;
+}
+
+interface User {
+  id: number;
+  first_name: string;
+}
+
+interface Produto{
+  id: number;
+  quantity: number;
+  price: string;
+  length: number;
+}
+
+function formatarData(dataFormat: string) {
+  if (dataFormat !== null) {
+    const parsedDate = parseISO(dataFormat);
+    return format(parsedDate, 'dd/MM/yyyy', { locale: ptBR });
+  }
+}
+
+function calcularTotalVenda(val: any) {
+  const totalProdutos = val.products.reduce((total: any, produto: any) => {
+    // return total + produto.quantity * parseFloat(produto.price);
+    return total + parseFloat(produto.price);
+  }, 0);
+
+  const desconto = parseFloat(val.discount);
+
+  const totalVenda = totalProdutos - desconto;
+
+  // return totalVenda.toFixed(2); // Formata o total com duas casas decimais
+  return totalVenda; // Formata o total com duas casas decimais
+}
+
 
 export default function Orders() {
   const [openModal, setOpenModal] = useState(false);
   const [openModalDetails, setOpenModalDetails] = useState(false);
-  const [openModalConfirm, setOpenModalConfirm] = useState(true);
+  const [loadingModal, setLoadingModal] = useState(false);
+  const [items, setItems] = useState<IData[] | null>(null);
 
-  return (
-    <S.Container>
-      <S.Content>
-        <S.Title>
-          <span>
-            Venda{'>'}
-            <small>Todos as vendas</small>
-          </span>
-          <S.Header>
-            <NewItem
-              url="/Orders/Create"
-              icon={<CiCirclePlus fontSize={24} />}
-              title="Novo"
-            />
-          </S.Header>
-        </S.Title>
-        <S.BodyTable>
-          <tr>
-            <th>Resumo</th>
-            <th>Data</th>
-            <th>Produto</th>
-            <th>QTD</th>
-            <th>Cliente</th>
-            <th>Ações</th>
-          </tr>
-          <tr>
-            <td>900,00 R$</td>
-            <td>12/12/2023</td>
-            <td>Bolsa</td>
-            <td>3</td>
-            <td>Fabine Luiza</td>
-            <td>
-              <div>
-                <button>
-                  <PiClipboardTextThin
-                    onClick={() => setOpenModalDetails(true)}
-                  />
-                </button>
-                <button>
-                  <CiEdit />
-                </button>
-                <button onClick={() => setOpenModal(true)}>
-                  <CiTrash />
-                </button>
-              </div>
-            </td>
-          </tr>
-        </S.BodyTable>
-      </S.Content>
+  //Paginas
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [perPage, setPerPage] = useState();
+  const [lastPage, setLastPage] = useState();
 
-      <S.Footer>
-        <Pagination
-          currentPage={1}
-          lastPage={undefined}
-          perPage={undefined}
-          prevPage={() => console.log('ola')}
-          nextPage={() => console.log('ola')}
+  const goToNextPage = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+  };
+
+  const goToPrevPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoadingModal(true);
+      try {
+        let url = 'order?page=' + currentPage;
+
+        const response = await axios_product.get(`v1/${url}`);
+        if (response.data.data.length > 0) {
+          setItems(response.data.data);
+          setTotalPages(response.data.last_page);
+          setPerPage(response.data.per_page);
+          setLastPage(response.data.last_page);
+        }
+
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoadingModal(false);
+      }
+    };
+    fetchData();
+  }, [currentPage]);
+
+  if (items) {
+    return (
+      <S.Container>
+        {loadingModal && (<Loading />)}
+        <S.Content>
+          <S.Title>
+            <span>
+              Venda{'>'}
+              <small>Todos as vendas</small>
+            </span>
+            <S.Header>
+              <NewItem
+                url="/Orders/Create"
+                icon={<CiCirclePlus fontSize={24} />}
+                title="Novo"
+              />
+            </S.Header>
+          </S.Title>
+          <S.BodyTable>
+            <tr>
+              <th>#</th>
+              <th>Resumo</th>
+              <th>Cliente</th>
+              <th>Data</th>
+              <th>Vendedor</th>
+              <th>Ações</th>
+            </tr>
+            {
+              items.map((val, index) => {
+                const totalVenda = calcularTotalVenda(val);
+                console.log(val);
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{totalVenda} R$</td>
+                    <td>{val.customer.name}</td>
+                    <td>{formatarData(val.created_at)}</td>
+                    <td>{val.user.first_name}</td>
+                    <td>
+                      <div>
+                        <button>
+                          <PiClipboardTextThin
+                            onClick={() => setOpenModalDetails(true)}
+                          />
+                        </button>
+                        <button>
+                          <CiEdit />
+                        </button>
+                        <button onClick={() => setOpenModal(true)}>
+                          <CiTrash />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+
+                )
+              })
+            }
+          </S.BodyTable>
+        </S.Content>
+
+        <S.Footer>
+          <Pagination
+            currentPage={currentPage}
+            lastPage={lastPage}
+            perPage={perPage}
+            prevPage={goToPrevPage}
+            nextPage={goToNextPage}
+          />
+        </S.Footer>
+        {/* Modals */}
+        <Modal
+          itemId={1}
+          isOpen={openModal}
+          setModalOpen={() => setOpenModal(false)}
+          url='order'
         />
-      </S.Footer>
-      {/* Modals */}
-      <Modal
-        itemId={1}
-        isOpen={openModal}
-        setModalOpen={() => setOpenModal(false)}
-        url='order'
-      />
-      <ModalDetails
-        itemId={1}
-        isOpen={openModalDetails}
-        setModalOpen={() => setOpenModalDetails(false)}
-      />
+        <ModalDetails
+          itemId={1}
+          isOpen={openModalDetails}
+          setModalOpen={() => setOpenModalDetails(false)}
+        />
 
-      {/* <ModalConfirm
+        {/* <ModalConfirm
         icon={<CiCircleInfo />}
         title='Confirmado'
         isOpen={openModalConfirm}
         setModalOpen={() => setOpenModalConfirm(false)}
       /> */}
-    </S.Container>
-  );
+      </S.Container>
+    );
+  }
+  return null;
 }
